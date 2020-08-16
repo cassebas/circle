4f0110cfd0223ea59ba34c5f10be1e7a43ad460b
Caspar Treijtel <c.treijtel@xs4all.nl>
2020-08-16 15:14:04 +0200

    Add target for installation for pi3

--- app/corunners/Install.mk
+++ app/corunners/Install.mk
@@ -2,3 +2,6 @@ CFLAGS += $(SYNBENCH_DATASIZE) $(BENCHMARK_CONFIG)
 
 install: $(TARGET).img
 	cp -p $< /home/caspar/git/Raspberry-Pi-Networkboot/tftp/ac558ec1/
+
+install3: $(TARGET).img
+	cp -p $< /home/caspar/git/Raspberry-Pi-Networkboot/tftp/bcab9c80/
